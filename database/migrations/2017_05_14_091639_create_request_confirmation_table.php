c44a2270c9c4d80a3e7016534f700740322af6d3
Victor Edoardo <victoredoardo@gmail.com>
2017-05-14 06:46:32 -0300

    Create request_confirmation table in migration scripts

---
+++ database/migrations/2017_05_14_091639_create_request_confirmation_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateRequestConfirmationTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('request_confirmation', function (Blueprint $table) {
+                $table->string('id_user_req_sign', 1)->default('N');
+                $table->string('id_user_assign_sign', 1)->default('N');
+                $table->date('dt_user_req_sign')->default(null);
+                $table->date('dt_user_assign_sign')->default(null);
+                $table->timestamps();
+                $table->integer('lst_chg_by')->nullable();
+                $table->integer('id_del')->default(0);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}

c2195c775ca7f4b26f2f1330ce335cba09810c1b
rdash <thiago.nobayashi@gmail.com>
2017-05-14 00:41:06 -0300

    Master seed done

---
+++ database/seeds/MasterUserTableSeed.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class MasterUserTableSeed extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('users')->insert([
+            'email' => "nagoya@frito.com",
+            'password' => bcrypt('secret'),
+            'id_cat' => 4,
+            'lst_chg_by' => 1,
+            'id_del' => 1,
+        ]);
+
+        DB::table('users')->insert([
+            'email' => "master@yoda.com",
+            'password' => bcrypt('secret'),
+            'id_cat' => 4,
+            'lst_chg_by' => 1,
+            'id_del' => 1,
+        ]);
+
+        DB::table('users')->insert([
+            'email' => "rem@ram.com",
+            'password' => bcrypt('secret'),
+            'id_cat' => 4,
+            'lst_chg_by' => 1,
+            'id_del' => 1,
+        ]);
+    }
+}

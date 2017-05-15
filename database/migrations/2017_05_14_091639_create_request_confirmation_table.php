6d73346be45a87009c47a880a6ce37fd37074639
Victor Edoardo <victoredoardo@gmail.com>
2017-05-15 03:36:45 -0300

    Fixed issues with unprepared DB statements and triggers syntax Added trigger to change status_req on request to ACPT once a new row gets added in request_assignment Add drop of new trigger

--- database/migrations/2017_05_14_091639_create_request_confirmation_table.php
+++ database/migrations/2017_05_14_091639_create_request_confirmation_table.php
@@ -16,8 +16,8 @@ class CreateRequestConfirmationTable extends Migration
         Schema::create('request_confirmation', function (Blueprint $table) {
                 $table->string('id_user_req_sign', 1)->default('N');
                 $table->string('id_user_assign_sign', 1)->default('N');
-                $table->date('dt_user_req_sign')->default(null);
-                $table->date('dt_user_assign_sign')->default(null);
+                $table->date('dt_user_req_sign')->default(null)->nullable();
+                $table->date('dt_user_assign_sign')->default(null)->nullable();
                 $table->timestamps();
                 $table->integer('lst_chg_by')->nullable();
                 $table->integer('id_del')->default(0);

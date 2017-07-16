87bbf6a771df18d831bac0dc1c48b8f6378c7637
Victor Edoardo <victoredoardo@gmail.com>
2017-07-15 22:49:43 -0300

    Creating archive tables for requests to be pushed to after completion or cancellation

---
+++ database/migrations/2017_07_16_010447_create_request_arc_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateRequestArcTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('request_arc', function (Blueprint $table) {
+            $table->date('dt_arc');
+            $table->integer('id_req');
+            $table->string('desc_req');
+            $table->string('mod_req');
+            $table->string('status_garbage');
+            $table->string('status_req');
+            $table->string('id_active', 1);
+            $table->date('dt_collect');
+            $table->integer('id_user_req');
+            $table->integer('id_garbage');
+            $table->timestamps();
+            $table->integer('lst_chg_by')->nullable();
+            $table->integer('id_del');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
---
+++ database/migrations/2017_07_16_011652_create_request_conf_arc_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateRequestConfArcTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('request_confirmation', function (Blueprint $table) {
+                $table->date('dt_arc');
+                $table->integer('id_req');
+                $table->string('id_user_req_sign', 1);
+                $table->string('id_user_assign_sign', 1);
+                $table->date('dt_user_req_sign')->default(null)->nullable;
+                $table->date('dt_user_assign_sign')->default(null)->nullable;
+                $table->timestamps();
+                $table->integer('lst_chg_by')->nullable();
+                $table->integer('id_del');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}

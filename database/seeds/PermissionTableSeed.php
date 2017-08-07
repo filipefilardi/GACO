6b4a9a0ff18847576242e7c26e7ed319cd6dd891
Victor Edoardo <victoredoardo@gmail.com>
2017-08-06 23:51:39 -0300

    Adding permission to cancel requests

--- database/seeds/PermissionTableSeed.php
+++ database/seeds/PermissionTableSeed.php
@@ -11,6 +11,8 @@ class PermissionTableSeed extends Seeder
      */
     public function run()
     {
+
+        /////////////////////////////////////////// USER CAT 1
         DB::table('permission')->insert([
         	'id_cat' => "1",
             'nm_perm' => "create_request",
@@ -26,7 +28,14 @@ class PermissionTableSeed extends Seeder
             'nm_perm' => "confirm_request",
             'desc_perm' => "this permission allows to confirm a given request",
         ]);
+        DB::table('permission')->insert([
+            'id_cat' => "1",
+            'nm_perm' => "cancel_request",
+            'desc_perm' => "this permission allows to cancel a given request",
+        ]);
 
+
+        /////////////////////////////////////////// USER CAT 2
         DB::table('permission')->insert([
         	'id_cat' => "2",
             'nm_perm' => "create_request",
@@ -42,8 +51,14 @@ class PermissionTableSeed extends Seeder
             'nm_perm' => "confirm_request",
             'desc_perm' => "this permission allows to confirm a given request",
         ]);
+        DB::table('permission')->insert([
+            'id_cat' => "2",
+            'nm_perm' => "cancel_request",
+            'desc_perm' => "this permission allows to cancel a given request",
+        ]);
 
 
+        /////////////////////////////////////////// USER CAT 3 - COOP
         DB::table('permission')->insert([
         	'id_cat' => "3",
             'nm_perm' => "view_all_requests",
@@ -59,20 +74,23 @@ class PermissionTableSeed extends Seeder
             'nm_perm' => "accept_requests",
             'desc_perm' => "this permission allows to accept a given request",
         ]);
+        DB::table('permission')->insert([
+            'id_cat' => "3",
+            'nm_perm' => "cancel_request",
+            'desc_perm' => "this permission allows to cancel a given request",
+        ]);
 
+        ////////////////////////////////////////// USER CAT 4 - MASTER
         DB::table('permission')->insert([
         	'id_cat' => "4",
             'nm_perm' => "view_all_requests",
             'desc_perm' => "this permission allows to view all requests",
         ]);
-
-
         DB::table('permission')->insert([
         	'id_cat' => "4",
             'nm_perm' => "create_user",
             'desc_perm' => "this permission allows to create users",
         ]);
-
         DB::table('permission')->insert([
         	'id_cat' => "4",
             'nm_perm' => "create_request",

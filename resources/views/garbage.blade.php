bd16dd470d4cb43a92c6010a9ac77464d026ae0d
filipefilardi <filipefilardi@hotmail.com>
2017-08-16 00:07:55 -0300

    Added few texts

--- resources/views/garbage.blade.php
+++ resources/views/garbage.blade.php
@@ -3,35 +3,41 @@
 @section('content')
 <div class="container">
 	<div class="row">
-		<div class="col-md-8 col-md-offset-2">
+		<!-- <div class="col-md-8 col-md-offset-2"> -->
 			<div class="panel panel-default">
 				
 				<div class="panel-heading">@lang('app.ewaste')</div>
-				<div class="panel-body">
+				<div class="panel-body text-justify">
 				 	
-					 <p>@lang('app.whydonate')</p>
-					 
-					<img src="{{URL::asset('/img/picojaragua.jpg')}}" class="img-responsive" alt="Pico do Jaraguá">
 
-					<p class="text-center" style="color: black; margin-top: 5px;">@lang('app.jaraguapicture')</p>
+				<p>@lang('app.ewastetext')</p>
 
-					 <p class="text-justify">@lang('app.ewastetext')</p>
+				<div class="text-header">@lang('app.whydonate')</div>
+				<p>@lang('app.whydonatetext')</p>			
 
-					
-					 <a href="{{ url('/home') }}" class="btn btn-primary btn-block"  style="margin-bottom: 15px">@lang('app.wannacontribute')</a>
+				<img src="{{URL::asset('/img/picojaragua.jpg')}}" class="img-responsive" alt="Pico do Jaraguá">
+				<p class="text-center" style="color: black; margin-top: 5px;">@lang('app.jaraguapicture')</p>
 
-					 <p>@lang('app.discoveryallresidues')</p>
-					 <ul class="list-group">
-					  @foreach ($garbage as $garbage)
-					  	<li class="list-group-item">{{$garbage->nm_garbage}}</li>
-					  @endforeach
-					</ul>
+				<div class="text-header">@lang('app.health')</div>
+				@lang('app.healthtext')
+
+				<div class="text-header">@lang('app.responsabilitylaw')</div>
+				@lang('app.responsabilitylawtext')
+
+				<a href="{{ url('/home') }}" class="btn btn-primary btn-block"  style="margin-bottom: 15px">@lang('app.wannacontribute')</a>
+
+				<div class="text-header">@lang('app.discoveryallresidues')</div>
+				<ul class="list-group">
+					@foreach ($garbage as $garbage)
+						<a class="list-group-item text-center">{{$garbage->nm_garbage}}</a>
+					@endforeach
+				</ul>
 
 					<a href="{{ url('/home') }}" class="btn btn-primary btn-block">@lang('app.wannacontribute')</a>
 				</div>
 			
 			</div>
-		</div>
+		<!-- </div> -->
 	</div>
 </div>
 @endsection

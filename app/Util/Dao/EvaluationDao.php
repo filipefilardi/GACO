db8aeb291121716d07953cb3ccdbd37ce8e2b281
Victor Edoardo <victoredoardo@gmail.com>
2017-08-20 15:39:31 -0300

    Implementing joins for new Evaluation query behavior and removing id_coop necessity from methods

--- app/Util/Dao/EvaluationDao.php
+++ app/Util/Dao/EvaluationDao.php
@@ -8,7 +8,7 @@ class EvaluationDao
 
 {
     
-    public static function insert_evaluation($punctual, $satisf, $obs, $id_req, $id_coop)
+    public static function insert_evaluation($punctual, $satisf, $obs, $id_req)
     {
 
         // VALIDATION BLOCK //////////////
@@ -17,7 +17,6 @@ class EvaluationDao
         if(is_null($punctual) || $punctual <= 0)    array_push($errors, 'Punctuality score null or invalid;');
         if(is_null($satisf) || $satisf <= 0)        array_push($errors, 'Satisfaction score null or invalid;');
         if(is_null($id_req) || $id_req <= 0)        array_push($errors, 'id_req null or invalid;');
-        if(is_null($id_coop) || $id_coop <= 0)      array_push($errors, 'id_coop null or invalid;');
         
         // END VALIDATION BLOCK /////////
 
@@ -31,8 +30,7 @@ class EvaluationDao
             'punctual_eval' => $punctual,
             'satisf_eval' => $satisf,
             'observation' => $obs,
-            'id_req' => $id_req,
-            'id_user_coop' => $id_coop
+            'id_req' => $id_req
         ]);
     }
 
@@ -43,10 +41,12 @@ class EvaluationDao
                     ->select(DB::raw('  avg(punctual_eval)  AS punctual_eval, 
                                         avg(satisf_eval)    AS satisf_eval, 
                                         count(1)            AS count, 
-                                        id_user_coop'))
-                    ->where('id_del', 0)
-                    ->where('id_user_coop', $id_coop)
-                    ->groupBy('id_user_coop')
+                                        id_user_assign'))
+                    ->join('request_assignment', 'coop_evaluation.id_req', '=', 'request_assignment.id_req')
+                    ->where('request_assignment.id_user_assign', $id_coop)
+                    ->where('request_assignment.id_del', 0)
+                    ->where('coop_evaluation.id_del', 0)
+                    ->groupBy('id_user_assign')
                     ->first();
                     
         return $result;
@@ -56,8 +56,10 @@ class EvaluationDao
     {
 
         $result = DB::table('coop_evaluation')
-                    ->where('id_del', 0)
-                    ->where('id_user_coop', $id_coop)
+                    ->join('request_assignment', 'coop_evaluation.id_req', '=', 'request_assignment.id_req')
+                    ->where('request_assignment.id_user_assign', $id_coop)
+                    ->where('request_assignment.id_del', 0)
+                    ->where('coop_evaluation.id_del', 0)
                     ->pluck('observation');
                     
         return $result;

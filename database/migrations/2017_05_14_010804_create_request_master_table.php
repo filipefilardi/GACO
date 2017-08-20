521a4b1a44c7a08107a20422c01fc358735b1e6a
Victor Edoardo <victoredoardo@gmail.com>
2017-08-19 23:35:05 -0300

    Creating new master request table for composite request handling - not activated for the flows yet

---
+++ database/migrations/2017_05_14_010804_create_request_master_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateRequestMasterTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('request_master', function (Blueprint $table) {
+            $table->increments('id_req_master');
+            $table->string('status_req');
+            $table->string('conf_token');
+            $table->string('id_active', 1);
+            $table->date('dt_req');
+            $table->date('dt_collect')->nullable();
+            $table->timestamps();
+            $table->integer('lst_chg_by')->nullable();
+            $table->integer('id_del')->default(0);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}

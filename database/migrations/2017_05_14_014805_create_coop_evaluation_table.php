1dc7c6b882f7e13156fd7269835beed93e5c39b8
Victor Edoardo <victoredoardo@gmail.com>
2017-08-20 04:49:38 -0300

    Evaluation testing and developing Dao

--- database/migrations/2017_05_14_014805_create_coop_evaluation_table.php
+++ database/migrations/2017_05_14_014805_create_coop_evaluation_table.php
@@ -15,7 +15,7 @@ class CreateCoopEvaluationTable extends Migration
     {
         Schema::create('coop_evaluation', function (Blueprint $table) {
             $table->increments('id_eval');
-            $table->float('puctual_eval');
+            $table->float('punctual_eval');
             $table->float('satisf_eval');
             $table->string('observation', 140)->nullable();
             $table->integer('lst_chg_by')->nullable();

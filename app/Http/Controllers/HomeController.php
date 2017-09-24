27cf2b7ed3c375dd94faa9bfa489397fd4b9b17c
Victor Edoardo <victoredoardo@gmail.com>
2017-09-24 12:34:04 -0300

    Creating and passing to view master requests for coop

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -74,14 +74,35 @@ class HomeController extends Controller
                 }
             }
             
-            //dd($master_user_pend);
-            
-            $user_acpt = RequestDao::get_full_info_dashboard_req_by_user_conditional($id_user, 'request.status_req', '=', 'ACPT');
-            $user_pend = RequestDao::get_full_info_dashboard_req_by_user_conditional($id_user, 'request.status_req', '=', 'PEND');
+            #$user_acpt = RequestDao::get_full_info_dashboard_req_by_user_conditional($id_user, 'request.status_req', '=', 'ACPT');
+            #$user_pend = RequestDao::get_full_info_dashboard_req_by_user_conditional($id_user, 'request.status_req', '=', 'PEND');
+        
         }
+        
         else{
-            $request = RequestDao::get_pend_requests_for_coop();
-            $request_acpt = RequestDao::get_acpt_requests_by_coop($id_user);
+            $master_coop_pend = RequestMasterDao::get_master_conditional('status_req', '=', 'ACPT');
+            $master_coop_acpt = RequestMasterDao::get_master_acpt_by_coop($id_user);
+        
+            $count = 0;
+
+            if(sizeof($master_coop_acpt) > 0) {
+                foreach ($master_coop_acpt as $key => $value) {
+                    $coop_acpt = RequestDao::get_full_info_dashboard_req_conditional('request.id_req_master','=',$value->id_req_master);
+                    $count++;
+                    $master_coop_acpt->splice((int)$key + $count, 0, $coop_acpt );
+                }
+            }
+
+            $count = 0;
+
+            if(sizeof($master_coop_pend) > 0) {
+                foreach ($master_coop_pend as $key => $value) {
+                    $coop_pend = RequestDao::get_full_info_dashboard_req_conditional('request.id_req_master','=',$value->id_req_master);
+                    $count++;
+                    $master_coop_pend->splice((int)$key + $count, 0, $coop_pend );
+                }
+            }
+
         }
         
         if ($id_cat == 4) return redirect('admin');
@@ -89,10 +110,12 @@ class HomeController extends Controller
         // dd($master_user_pend);
         return view('home')->with('request', $request)
                            ->with('request_acpt', $request_acpt)
-                           ->with('user_acpt', $user_acpt)
-                           ->with('user_pend', $user_pend)
+                           #->with('user_acpt', $user_acpt)
+                           #->with('user_pend', $user_pend)
                            ->with('master_user_acpt', $master_user_acpt)
-                           ->with('master_user_pend', $master_user_pend);
+                           ->with('master_user_pend', $master_user_pend)
+                           ->with('master_coop_acpt', $master_coop_acpt)
+                           ->with('master_coop_pend', $master_coop_pend);
     }
 
     /**
